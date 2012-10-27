6b319cddb2473d31189595e4c21bf5b47e409264
gauravshetti <gauravshetti@gmail.com>
2012-10-27 00:45:22 -0700

    First file commit

---
+++ salecloud.py
@@ -0,0 +1,85 @@
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import tweepy
+import datetime,json,re,time
+import enchant
+from nltk import wordpunct_tokenize
+
+from multiprocessing import Pool
+# Go to http://dev.twitter.com and create an app. 
+# The consumer key and secret will be generated for you after
+consumer_key="hfCFxWaSxc5Tyjs0O1B7Cg"
+consumer_secret="ZpRAbm5cSo5QM3Al2vc0HE0qUny8RBXRwlFecOb4"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token="261074343-mya1UBLv4DFhCXnOv8hkkV5Jh6kZhb7Sff4oegK9"
+access_token_secret="y5YkAEOc3s6n4ETUrB9naXV737XvK5mpIqM5lWaLIZQ"
+
+
+class StdOutListener(StreamListener):
+	""" A listener handles tweets are the received from the stream. 
+	This is a basic listener that just prints received tweets to stdout.
+"""
+	i = 1
+	j = 1
+	#time = datetime.datetime.now()
+	fileObj = None
+	#pool = Pool(processes=5) 
+	#currTime = -1
+	pattern = re.compile('\$+\.*[0-9]+|\$+')
+	engCheck = enchant.Dict("en_US")
+	def on_data(self, data):
+		#print data
+		#self.fileObj.write(data)
+		result = self.analyzedata(data)
+		print 'sent the data'
+		return True
+
+	def on_error(self, status):
+		print status
+
+	def init(self):
+		self.fileObj = open('streamdata','a+')
+
+	def analyzedata(self,data):
+		json_data = json.loads(data)
+		tokens = wordpunct_tokenize(json_data['text'])
+		for i in range(0,len(tokens)):
+			eng = False
+			token = tokens[i].lower()
+			if token == 'deal' or token == 'sale' or token =='deals' or token =='sales' or self.pattern.match(token):
+				if i > 0:
+					eng = engCheck.check(tokens[i-1].lower())
+				elif i < len(tokens) - 1:
+					if token == 'deal':
+						eng = False if token[i+1].lower() == 'with' else True
+					else:
+						eng = engCheck.check(tokens[i+1].lower())
+				else:
+					eng = True
+				if eng == True:
+					self.fileObj.write(data)
+					self.fileObj.flush()
+					print 'processed an entry'
+					break
+
+
+if __name__ == '__main__':
+	l = StdOutListener()
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	l.init()
+	stream = Stream(auth, l)	
+	#time1 = datetime.datetime.now()
+	#data = stream.sample()
+	i = 1
+	while (i == 1):
+		try:
+			data = stream.filter(locations=[-122.75,36.8,-121.75,37.9])
+		except e:
+			time.sleep(10)
+	'''timediff = datetime.datetime.now() - time1
+	print timediff.seconds
+	print timediff'''
