c60b71bcfc7b2c02ceb7235b7136e6ba4b658881
arianshams <shams.arian@gmail.com>
2012-10-31 13:21:58 -0700

    frontend push

---
+++ frontend/data-template.js
@@ -0,0 +1,36 @@
+var data = {
+	
+	'buildtime': 'October 12, 2012 13:14:00',
+	'points':[
+		{
+			'pointid': 1,
+			'lat': 37.8084599,
+			'long': -122.4142845,
+			'color': '#d35f43',
+			'opacity': 0.7,
+			'size': 50,
+			'tweets': [
+				{
+					'tweetid':1234,
+					'screen_name': 'arian',
+					'time': 'October 12, 2012 13:14:00',
+					'text': 'Some tweet about sales' 		
+				},
+				{
+					'tweetid':1234,
+					'screen_name': 'arian2',
+					'time': 'October 12, 2012 13:14:00',
+					'text': 'tweet2' 		
+				},
+				{
+					'tweetid':1234,
+					'screen_name': 'arian3',
+					'time': 'October 12, 2012 13:14:00',
+					'text': 'tweet3' 		
+				},
+				
+			]//tweet
+		}
+	]//map
+
+};
